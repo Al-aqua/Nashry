35e43f238042bfe0608e265eded3fb77061df953
Mohammad Al-aqua <reiaqua0@gmail.com>
2022-10-21 18:53:19 +0300

    add Linq to Sql

--- Nashry/Properties/Settings.Designer.cs
+++ Nashry/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace Nashry.Properties {
                 return ((string)(this["dbConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Nashrydb.mdf;" +
+            "Integrated Security=True")]
+        public string NashrydbConnectionString {
+            get {
+                return ((string)(this["NashrydbConnectionString"]));
+            }
+        }
     }
 }
